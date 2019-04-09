77cf2b476d3bd8f070976a623a3f30dc9c43600f
Adigezalov <adigezalov.roman@gmail.com>
2019-04-09 11:45:54 +0500

    Автокомпиляция less

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -451,6 +451,17 @@ module.exports = function (webpackEnv) {
                                 'sass-loader'
                             ),
                         },
+
+                        {
+                            test: lessRegex,
+                            use: getStyleLoaders({
+                                    importLoaders: 2,
+                                    sourceMap: isEnvProduction && shouldUseSourceMap,
+                                },
+                                'less-loader'
+                            )
+                        },
+
                         // "file" loader makes sure those assets get served by WebpackDevServer.
                         // When you `import` an asset, you get its (virtual) filename.
                         // In production, they would get copied to the `build` folder.
